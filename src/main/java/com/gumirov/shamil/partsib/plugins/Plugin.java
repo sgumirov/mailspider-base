be3c92f9d497719a46f8cdbc055bbd24b0407bfa
Shamil Gumirov <shamilg1@gumirov.com>
2018-03-28 11:52:28 +0200

    1.9: Added temp file-related interface part and added throwing of Exception for Plugin.processFile()

--- src/main/java/com/gumirov/shamil/partsib/plugins/Plugin.java
+++ src/main/java/com/gumirov/shamil/partsib/plugins/Plugin.java
@@ -2,25 +2,21 @@ package com.gumirov.shamil.partsib.plugins;
 
 import org.slf4j.Logger;
 
+import java.io.File;
 import java.io.InputStream;
 
 /**
- * Plugin can change only file contents via returning new InputStream value from processFile(). <p>Note: it's recommended to do 
- * all the operations with streams in memory instead of disk to keep latency low. 
+ * Plugin can change only file contents via returning new InputStream value from processFile().
  * <p>
- *   Note: since version 1.8 if a plugin creates temporary file to be deleted when not used it must add File with
- *   key {@link #TEMP_FILE} to
- *   {@link FileMetaData#headers}
+ *   Note: since version 1.8 if a plugin creates temporary file to be deleted later it must add it to queue
+ *   using {@link FileMetaData#addFileToDelete(File)}
  * </p>
- * <p>(c) 2017 by Shamil Gumirov (shamil@gumirov.com).<br/>
+ * <p>(c) 2017 by Shamil Gumirov (shamil@gumirov.com).
+ * <p>
+ *   @since 1.8 Throws Exception in case of error.
+ * </p>
  */
 public interface Plugin {
-  /**
-   * Key for header to be used when plugin creates temp file to be deleted when not used. Value can be of type: File,
-   * List&lt;String&gt;.
-   * @since 1.8
-   */
-  public final String TEMP_FILE = "MAILSPIDER_PLUGIN_TEMP_FILE";
 
   /**
    * Processes file and maybe replaces its content with new one.
@@ -29,6 +25,7 @@ public interface Plugin {
    * @param metadata in params, see {@link FileMetaData} class for details. In/out param: possible to add new values.
    * @param log logger to use
    * @return new file contents, or null if no changes needed
+   * @throws Throwable if something wrong. Actually in case of trouble it's better to let it fall.
    */
-  InputStream processFile(FileMetaData metadata, Logger log);
+  InputStream processFile(FileMetaData metadata, Logger log) throws Exception;
 }
